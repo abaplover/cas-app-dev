9833c2557fde4e8fc8eb4d483b3ab9f44965a6b7
abaplover <roland1105@live.com>
2021-10-29 17:21:07 -0400

    Eliminar API KEy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,12 +15,7 @@ export const environment = {
     measurementId: "G-FFF4M4KJMF"
   }*/
   firebaseConfig: {
-    apiKey: "AIzaSyAGRVU2FXL9cI4NiusyAwdQyYD9tH8iVEM",
-    authDomain: "cloud-advanced-selling-dev.firebaseapp.com",
-    projectId: "cloud-advanced-selling-dev",
-    storageBucket: "cloud-advanced-selling-dev.appspot.com",
-    messagingSenderId: "940769672352",
-    appId: "1:940769672352:web:13de3e60b117efa7795dc5"
+
   }
 };
 
