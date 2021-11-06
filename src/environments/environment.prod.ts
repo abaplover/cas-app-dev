9b5681b75b1103f9ee6379ce88371ee8d341e5aa
Roland <roland1105@live.com>
2021-11-06 14:30:27 -0400

    Agregar configuracion de Build Pro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAGRVU2FXL9cI4NiusyAwdQyYD9tH8iVEM",
+    authDomain: "cloud-advanced-selling-dev.firebaseapp.com",
+    databaseURL: "https://cloud-advanced-selling-dev-default-rtdb.firebaseio.com",
+    projectId: "cloud-advanced-selling-dev",
+    storageBucket: "cloud-advanced-selling-dev.appspot.com",
+    messagingSenderId: "940769672352",
+    appId: "1:940769672352:web:13de3e60b117efa7795dc5"
+  }
 };
