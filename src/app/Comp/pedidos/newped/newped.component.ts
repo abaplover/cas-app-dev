3e55bed47889d9b8a8628baba7f9a43bdb8b9f17
Anyami Cornieles <anyamigcr@gmail.com>
2022-02-09 20:08:39 -0400

    Testing

--- src/app/Comp/pedidos/newped/newped.component.ts
+++ src/app/Comp/pedidos/newped/newped.component.ts
@@ -773,10 +773,10 @@ export class NewpedComponent implements OnInit {
         //this.pedidoService.pedido_.fechapedido = new Date(this.pedidoService.pedido_.fechapedido);
         
         
-        this.pedidoService.pedido_.fechapedido = new Date(this.pedidoService.start_time);
+        /* this.pedidoService.pedido_.fechapedido = new Date(this.pedidoService.start_time);
         this.pedidoService.pedido_.fechapedido.setDate(this.pedidoService.pedido_.fechapedido.getDate()+1);
         this.pedidoService.pedido_.fechapedido.setHours(ahora.getHours());
-        this.pedidoService.pedido_.fechapedido.setMinutes(ahora.getMinutes());
+        this.pedidoService.pedido_.fechapedido.setMinutes(ahora.getMinutes()); */
 
         
         this.pedidoService.pedido_.modificado = new Date;
