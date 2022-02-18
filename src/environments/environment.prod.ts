bcc88ec03c9c86f10cf799cdfd05c1d1d992d689
Anyami Cornieles <anyamigcr@gmail.com>
2022-02-18 08:36:42 -0400

    Base de datos de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -19,12 +19,20 @@ export const environment = {
   } */
   firebaseConfig: {
     apiKey: "AIzaSyDlyVa-85DeK4dyX6D9wS34O14vC3F4QHY",
-    authDomain: "cloud-anvanced-seliing.firebaseapp.com",
-    databaseURL: "https://cloud-anvanced-seliing.firebaseio.com",
-    projectId: "cloud-anvanced-seliing",
-    storageBucket: "cloud-anvanced-seliing.appspot.com",
-    messagingSenderId: "38633795965",
-    appId: "1:38633795965:web:a9ba68e3846ee9b7f15c14",
-    measurementId: "G-FFF4M4KJMF"
+
+  authDomain: "cloud-anvanced-seliing.firebaseapp.com",
+
+  databaseURL: "https://cloud-anvanced-seliing.firebaseio.com",
+
+  projectId: "cloud-anvanced-seliing",
+
+  storageBucket: "cloud-anvanced-seliing.appspot.com",
+
+  messagingSenderId: "38633795965",
+
+  appId: "1:38633795965:web:a9ba68e3846ee9b7f15c14",
+
+  measurementId: "G-FFF4M4KJMF"
+
   }
 };
