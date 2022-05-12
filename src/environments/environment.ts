96d7d3cd78ec6d9494916ef39d9ede1575040e2f
Anyami Cornieles <anyamigcr@gmail.com>
2022-05-12 17:02:45 -0400

    Integracion con produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-  apiKey: "AIzaSyAGRVU2FXL9cI4NiusyAwdQyYD9tH8iVEM",
+ /*  apiKey: "AIzaSyAGRVU2FXL9cI4NiusyAwdQyYD9tH8iVEM",
 
   authDomain: "cloud-advanced-selling-dev.firebaseapp.com",
 
@@ -17,8 +17,8 @@ export const environment = {
 
   messagingSenderId: "940769672352",
 
-  appId: "1:940769672352:web:13de3e60b117efa7795dc5"
-  /* apiKey: "AIzaSyDlyVa-85DeK4dyX6D9wS34O14vC3F4QHY",
+  appId: "1:940769672352:web:13de3e60b117efa7795dc5" */
+  apiKey: "AIzaSyDlyVa-85DeK4dyX6D9wS34O14vC3F4QHY",
 
   authDomain: "cloud-anvanced-seliing.firebaseapp.com",
 
@@ -32,7 +32,7 @@ export const environment = {
 
   appId: "1:38633795965:web:a9ba68e3846ee9b7f15c14",
 
-  measurementId: "G-FFF4M4KJMF" */
+  measurementId: "G-FFF4M4KJMF"
 
   }
   
